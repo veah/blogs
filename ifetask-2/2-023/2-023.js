4b146bbddff46c6b93a35bc5a17cc5a26ea48c8d
veah <391821744@qq.com>
2016-04-12 18:04:01 +0800

    update

--- ifetask-2/2-023/2-023.js
+++ ifetask-2/2-023/2-023.js
@@ -1,5 +1,6 @@
 var queue = [];
-var ttt = null;
+var tempNode = null;
+var current,timer;
 var root = document.getElementById("root");
 var btn  = document.getElementById("btn");
 
@@ -10,16 +11,17 @@ function BFS(node){
 		temp.push(node);
 	}
 	while(temp.length>0){
-		var ttt = temp.shift()
-		queue.push(ttt);
-		if(ttt.firstElementChild){
-			ttt = ttt.firstElementChild;
-			temp.push(ttt);
+		var tempNode = temp.shift()
+		queue.push(tempNode);
+		if(tempNode.firstElementChild){
+			tempNode = tempNode.firstElementChild;
+			temp.push(tempNode);
+			while(tempNode.nextElementSibling){
+				tempNode = tempNode.nextElementSibling;
+				temp.push(tempNode);
+			}
 		}
-		while(ttt.nextElementSibling){
-			ttt = ttt.nextElementSibling;
-			temp.push(ttt);
-		}
+		
 	}
 
 }
@@ -30,13 +32,55 @@ function preOrder(node){
 		queue.push(node);
 		preOrder(node.firstElementChild);
 		if(node.firstElementChild){
-			ttt = node.firstElementChild.nextElementSibling;
-			 while(ttt){
-			 	p = ttt;
-			 	preOrder(ttt);
-			 	ttt = p.nextElementSibling;
+			tempNode = node.firstElementChild.nextElementSibling;
+			 while(tempNode){
+			 	p = tempNode;
+			 	preOrder(tempNode);
+			 	tempNode = p.nextElementSibling;
 			}
 		}
+	}
+}
+
+function showOut(){
+	current = queue.shift(); //FIFO
+	if(current){
+		current.style.backgroundColor = '#00B2EE';
+		timer = setTimeout(function(){
+			current.style.backgroundColor = '#fff';
+			showOut();
+		},750);
+	}
+}
+
+function showResult(){
+	if (queue.length === 0 && !found) {
+	    alert("没有找到");
+	}
+	head = queue.shift(); //出队
+	if (head) {
+	    text = head.firstChild.nodeValue;
+	    if (trim(text) === search.value) {
+	        head.style.backgroundColor = "deeppink";
+	        found = true;
+	        endTime = new Date();
+	        alert("Bingo！本次查询时间：" + (endTime - startTime) / 1000 + "s");
+	        return;
+	    } else {
+	        head.style.backgroundColor = "#6fa3ff";//显示蓝色
+	        timer = setTimeout(function () {
+	            head.style.backgroundColor = "#fff";//1秒后节点的蓝色变为白色
+	            searchShow(); //递归调用，使要显示的节点不停出队显示，直至为空
+	        }, 800);
+	    }
+	}
+}
+
+function show(){
+	if(search.value !== "") {
+	   showResult();
+	} else {
+	   showOut();
 	}
 }
 
@@ -44,7 +88,7 @@ btn.onclick = function (x){
 				if (queue.length>0){
 					current.style.backgroundColor = '#fff'
 					queue = [];
-					ttt= null;
+					tempNode= null;
 					clearTimeout(timer);
 				}
 				//判断点击按钮类型
@@ -55,10 +99,7 @@ btn.onclick = function (x){
 					case "BFS" : 					
 						BFS(root);
 						break;
-					case "post" : 					
-						postOrder(root);			
-						break;
-					}
+				}
 
-				// showOut();
+				show();
 			};
