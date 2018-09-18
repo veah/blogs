ddb176813207aaaf3fc256c44a2148682cf03263
huangyiwei <huangyiwei@kuaishou.com>
2018-09-18 18:00:36 +0800

    rebuild

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-    configureWebpack: {
-        build: {
-            assetsPublicPath: './'
-        }
-    }
+    baseUrl: process.env.NODE_ENV === 'production'
+        ? './'
+        : '/'
 }
